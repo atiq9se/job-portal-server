da171674f202f7e723dce69c1c5977b558ff428f
Md.Atiqur Rahman <atiq9se@gmail.com>
2024-12-20 01:19:17 +0600

    jobs details load in details page

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors  = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -40,6 +40,14 @@ async function run() {
        const cursor = jobsCollection.find();
        const result = await cursor.toArray();
        res.send(result)
+    })
+
+    app.get('/jobs/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
+
     })
 
 
